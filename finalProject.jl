4f810397d01438cb8199db18a10410fa3c916d62
Kaan Oguzhan <kaan135@gmail.com>
2016-04-16 03:58:10 +0300

    Reading handicap coordinates - half

--- finalProject.jl
+++ finalProject.jl
@@ -15,17 +15,38 @@ function getHandicapNumber(file)
 end
 
 
+function getHandicapCoordinates(lines)
+	hnNumb = Int(getHandicapNumber(lines))-48
+	
+	# Exit if there is no handicap
+	if hnNumb == 0
+		return 0
+	end
 
+	coords = zeros(2,hnNumb)
+
+	if hnNumb == 1
+		hnCoor = filter(x -> contains(x, "AB"),lines)[1] # Filtering handicap coordinates line
+		coords[1:2]=LetToNum(hnCoor[4:5])
+		return coords
+	end 
+	
+
+end
+
+function LetToNum(letCoor)
+	numCoor = zeros(1,2)
+	setindex!(numCoor, Int(letCoor[1])-96, 1)
+	setindex!(numCoor, Int(letCoor[2])-96, 2)
+end
 
 
 # Read file line by line
-file = open("2015-05-01-1.sgf")
+file = open("KnetAlphaGO/Dataset/2015-05-01-1.sgf")
 lines = readlines(file)
 
 # Generate 19x19 board
 board = zeros(19,19)
 
 
-getHandicapNumber(lines)
-
-getHandicapCoordinates()
+getHandicapCoordinates(lines)
