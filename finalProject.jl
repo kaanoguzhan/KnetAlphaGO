67a994300a73ac943215a720dc4ad13f78b2cf7a
Kaan Oguzhan <kaan135@gmail.com>
2016-04-18 19:18:00 +0300

    Playing game on board with rules - start
    - Board position numbers changed
    - Play function started

--- finalProject.jl
+++ finalProject.jl
@@ -14,8 +14,12 @@ function letToCoor(letCoor)
 	Coor = map(x -> Int(x) ,Coor)
 end
 
-function CoorContainer()
+function CoorContainer(;coor=map(x -> Int(x),zeros(1,2)))
 	Container = []
+
+	if coor != nothing
+		push!(Container,coor)
+	end
 end
 
 function addCoordinate(container,coordinate)
@@ -98,30 +102,72 @@ function getHandicapCoordinates(fileName)
 	return CoorCont
 end
 
+# # # # # # # # # # # # # #		Board position Numbers	# # # # # # # # # # # # # #
+
+function getPlayerNumber(player)
+	if player == 'B' 
+		return 1
+	elseif player == 'W'
+		return 2
+	end
+end
+
+function getOpponentNumber(player)
+	if player == 'B' 
+		return 2
+	elseif player == 'W'
+		return 1
+	end
+end
+
+function getNeutralNumber()
+	return 0
+end
+
 # # # # # # # # # # # # # #		Playing Moves			# # # # # # # # # # # # # #
 
 # player = N -> remove any stone from coordinate(s)
 # player = W -> Place white stone to coordinate(s)
 # player = B -> Place black stone to coordinate(s)
-function coordToBoard(board,coords; player = 'N')
+function coordToBoard(board,coords,player)
 	loopSize = size(coords)[1]
 
-	value = 0
-	if player == 'B' 
-		value = 1
-	elseif player == 'W'
-		value = 2
-	end
+	plNum = getPlayerNumber(player)
 
 	for i = 1 : loopSize
 		if coords[i][1] != 0 && coords[i][2] != 0
-			board[coords[i][1],coords[i][2]] = value
+			board[coords[i][1],coords[i][2]] = plNum
 		end
 	end
 end
 
 function play(board,coordinate,player)
-	# TODO
+	
+	x = coordinate[1][1]
+	y = coordinate[1][2]
+	
+	plNum = getPlayerNumber(player)
+
+	# Check if move is played on a neutral square
+	if board[x,y] != 0
+		return "Error: Move not possible !"
+	end
+
+	# Playing on a square that has all neutral around
+	if	board[ x+1 <= 19 ? x+1 : x , y ] == 0 &&
+		board[ x-1 >= 1  ? x-1 : x , y ] == 0 &&
+		board[ x , y+1 <= 19 ? y+1 : y ] == 0 &&
+		board[ x , y-1 >= 1  ? y-1 : y ] == 0
+			coordToBoard(board,coordinate,player)
+	end
+
+	# Playing on a square that has PROBLEM!
+		getNonNeurtalNeigbours(board,coordinate)
+
+end
+
+function getNonNeurtalNeigbours(board,coordinate)
+	#TODO
 end
 
 # # # # # # # # # # # # # #		Reading Player Moves	# # # # # # # # # # # # # #
@@ -212,15 +258,9 @@ end
 function getIFP(board, player)
 	IFP = zeros(19,19,48)	# Initialize empty Input feature plane
 
-	ply = 0
-	opp = 0
-	if player == 'B' 
-		ply = 1
-		opp = 2
-	elseif player == 'W'
-		ply = 2
-		opp = 1
-	end
+	ply = getPlayerNumber(player)
+	opp = getOpponentNumber(player)
+	neu = getNeutralNumber()
 
 	# 1		Player Stones
 	IFP[:,:,1] = map(x -> x == ply ? 1 : 0 ,board)
@@ -229,10 +269,12 @@ function getIFP(board, player)
 	IFP[:,:,2] = map(x -> x == opp ? 1 : 0 ,board)
 
 	# 3		Neutral/Empty Stones
-	IFP[:,:,3] = map(x -> x == 0   ? 1 : 0 ,board)
+	IFP[:,:,3] = map(x -> x == neu ? 1 : 0 ,board)
 
 	# 4		Ones
 	IFP[:,:,4] = map(x -> 1,board)
+
+	# 5		Turns Since
 
 	return IFP
 end
@@ -254,6 +296,14 @@ blackMoves = getBlackMoves(fileN)
 
 coordToBoard(board,hndCoords,player = 'W')
 IFP = getIFP(board, 'W')
+
+coordToBoard(board,hndCoords,player = 'B')
+
+play(board,CoorContainer(coor=numToCoor("12")),'W')
+coordToBoard(board,CoorContainer(coor=numToCoor("22")),'B')
+play(board,CoorContainer(coor=numToCoor("32")),'W')
+play(board,CoorContainer(coor=numToCoor("21")),'W')
+play(board,CoorContainer(coor=numToCoor("23")),'W')
 
 writedlm("Documents/KnetAlphaGO/board.txt", board)
 #board2 = readdlm("Documents/KnetAlphaGO/test.txt")
